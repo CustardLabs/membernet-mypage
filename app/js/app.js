0d4f1bc2bbf2a7a104c273fde2b8b62769df879f
Russ Flynn <russ@redpill-linpro.com>
2014-07-03 19:56:30 +0200

    Add basic retireval and display of profile data from MemberNet connector

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
@@ -13,4 +12,8 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]).
+config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,15 @@ angular.module('myApp.controllers', [])
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+  }])
+  
+  .controller("MemberProfileCtrl", function($scope, $http) {
+    $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
+    var url = "http://membernet/api/get/profile";
+    $http.get(url).
+    success(function(data, status, headers, config) {
+      console.debug(data, headers);
+      $scope.url = url; 
+      $scope.from_membernet = JSON.stringify(data);
+    });
+  });
