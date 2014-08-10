797270608441b5da262261244160dff098e6501c
Russ Flynn <russ@redpill-linpro.com>
2014-08-11 00:38:34 +0200

    Add login page and basic JWT injection/authentication

--- app/js/app.js
+++ app/js/app.js
@@ -12,9 +12,30 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/profile', {templateUrl: 'partials/profile.html', controller: 'ProfileCtrl'});
   $routeProvider.when('/activities', {templateUrl: 'partials/activities.html', controller: 'ActivitiesCtrl'});
   $routeProvider.when('/invoices', {templateUrl: 'partials/invoices.html', controller: 'InvoicesCtrl'});
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginCtrl'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]).
 config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
+}])
+.factory('authInterceptor', function ($rootScope, $q, $window) {
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        // handle the case where the user is not authenticated
+      }
+      return response || $q.when(response);
+    }
+  };
+})
+.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,13 +5,13 @@
 angular.module('myApp.controllers', [])
   .controller("ProfileCtrl", function($scope, $http) {
     $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
-    var url = "http://membernet/api/get/profile";
+    var url = "http://route.scoutnet.c.bitbit.net/test/demo/api/get/profile";
     $http.get(url).
     success(function(data, status, headers, config) {
       console.debug(data, headers);
       $scope.url = url; 
       $scope.from_membernet = JSON.stringify(data);
-    })
+    });
   })
   .controller('ActivitiesCtrl', ['$scope', function($scope) {
 
@@ -28,5 +28,24 @@ angular.module('myApp.controllers', [])
     $scope.isActive = function(item) {
       if (item.path === $location.path()) { return true; } else { return false; };
     };
-      
+
+  }])
+  .controller('LoginCtrl', ["$scope", "$http", "$window", function ($scope, $http, $window) {
+    $scope.user = {username: 'john.doe', password: 'foobar'};
+    $scope.message = '';
+    $scope.submit = function () {
+      $http
+        .post('http://route.scoutnet.c.bitbit.net/test/demo/api/authenticate', $scope.user)
+        .success(function (data, status, headers, config) {
+          $window.sessionStorage.token = data.token;
+          $scope.message = 'Welcome';
+        })
+        .error(function (data, status, headers, config) {
+          // Erase the token if the user fails to log in
+          delete $window.sessionStorage.token;
+
+          // Handle login errors here
+          $scope.message = 'Error: Invalid user or password';
+        });
+    };
   }]);
